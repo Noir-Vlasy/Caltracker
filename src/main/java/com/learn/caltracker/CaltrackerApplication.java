aed6f06727ffdc114718e52456afcecf08e829e1
Shiv <Shivanshp705@gmail.com>
2026-06-01 12:51:15 +0530

    Not Needed, laptop might crash so uploading for safety

---
+++ src/main/java/com/learn/caltracker/CaltrackerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.caltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaltrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaltrackerApplication.class, args);
+    }
+
+}
